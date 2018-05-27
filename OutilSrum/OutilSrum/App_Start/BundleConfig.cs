f999c4af672b0e608cc844a9712a8379f4fde79d
Mayzrane-Hassan <mayzranehassan@gmail.com>
2018-05-27 16:12:09 +0000

    Template files (css/js/images)

--- OutilSrum/OutilSrum/App_Start/BundleConfig.cs
+++ OutilSrum/OutilSrum/App_Start/BundleConfig.cs
@@ -25,6 +25,54 @@ namespace OutilSrum
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+
+
+            // template one ::
+            //styles ::
+            bundles.Add(new StyleBundle("~/Content/TemplateOne").Include(
+                     "~/Content/templateOne/bootstrap.min.css",
+                     "~/Content/templateOne/animate.min.css",
+                     "~/Content/templateOne/et-line.css",
+                     "~/Content/templateOne/icofont.css",
+                     "~/Content/templateOne/fontawesome-all.min.css",
+                     "~/Content/templateOne/YouTubePopUp.css",
+                     "~/Content/templateOne/magnific-popup.css",
+                     "~/Content/templateOne/owl.carousel.min.css",
+                     "~/Content/templateOne/owl.theme.default.min.css",
+                     "~/Content/templateOne/helper.css",
+                     "~/Content/templateOne/style.css",
+                     "~/Content/templateOne/custom.css",
+                     "~/Content/popup/mesStyles.css",
+                     "~/Content/templateOne/fontGoogleApi.css"
+                     ));
+
+            //scripts ::
+            bundles.Add(new ScriptBundle("~/bundles/TemplateOne").Include(
+                    "~/Scripts/TemplateOne/jquery-3.0.0.min.js",
+                    "~/Scripts/TemplateOne/jquery-migrate-3.0.0.min.js",
+                    "~/Scripts/TemplateOne/popper.min.js",
+                    "~/Scripts/TemplateOne/bootstrap.min.js",
+                     "~/Scripts/TemplateOne/scrollIt.min.js",
+                     "~/Scripts/TemplateOne/animated.headline.js",
+                     "~/Scripts/TemplateOne/jquery.waypoints.min.js",
+                     "~/Scripts/TemplateOne/jquery.counterup.min.js",
+                     "~/Scripts/TemplateOne/owl.carousel.min.js",
+                     "~/Scripts/TemplateOne/jquery.magnific-popup.min.js",
+                     "~/Scripts/TemplateOne/jquery.stellar.min.js",
+                     "~/Scripts/TemplateOne/isotope.pkgd.min.js",
+                     "~/Scripts/TemplateOne/YouTubePopUp.jquery.js",
+                     "~/Scripts/TemplateOne/map.js",
+                     "~/Scripts/TemplateOne/validator.js",
+                     "~/Scripts/TemplateOne/scripts.js"
+                     ));
+
+            // popup script
+            bundles.Add(new ScriptBundle("~/bundles/popupScripts").Include(
+                    "~/Scripts/popup/popup.js"
+                     ));
+
+
         }
     }
 }
